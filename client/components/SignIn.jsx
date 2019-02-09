d3d7efcacdddb812fb78754b8f7f876378f94a38
Franklin Pinnock <pinnockg@gmail.com>
2019-02-08 22:35:48 -0800

    This closes #6, #7, and #20

---
+++ client/components/SignIn.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function SignIn() {
+
+  return (
+    <div>
+      Sign in Component
+    </div>
+  )
+}
+
+export default SignIn;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const config = require('./../webpack.config.js');
-const compiler = webpack(config);
+const esprima = require('esprima');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const request = require('request');
-//const sequelize = require('sequelize')
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const config = require('./../webpack.config.js');
+
+const compiler = webpack(config);
+
+
+// const sequelize = require('sequelize')
 
 const userController = require('./../db/user/userController');
 const sessionController = require('./session/sessionController');
@@ -14,38 +18,37 @@ const cookieController = require('./util/cookieController');
 
 const app = express();
 
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath
-  })
-);
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+}));
 
-///////////////////////////////
-//connect to Postgres database here
-////////////////////////////
+// /////////////////////////////
+// connect to Postgres database here
+// //////////////////////////
 
-//app.set(//insert react);
+// app.set(//insert react);
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Render to index file
-// app.get('/', cookieController.setCookie, (req, res) => {
-//   res.render('./../client/index');
-// });
+app.get('/', cookieController.setCookie, (req, res) => {
+  res.render('./../client/index');
+});
 
 // Render to signup page
 app.get('/signup', (req, res) => {
   res.render('./../client/signup', { error: null });
 });
 
-//Post to signup page
+// Post to signup page
 // app.post('/signup', userController.createUser, sessionController.createJwt);
 
-// //Post to login page
+// Post to login page
 // app.post('/login', userController.verifyUser);
 
-// //able to see only if logged in
+// able to see only if logged in
 // app.get('/isLoggedin', sessionController.checkJwt, (req, res) => {
 //   userController.getAllUsers((err, users) => {
 //     if (err) throw err;
@@ -53,32 +56,47 @@ app.get('/signup', (req, res) => {
 //   });
 // });
 
-//Oauth process
+// Oauth process
 app.get('/oauth', (req, res) => {
   const code = req.query.code;
-  //const clientID = add client ID;
-  //const secret = add client secret;
+  // const clientID = add client ID;
+  // const secret = add client secret;
 
   request.post(
     `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${secret}&code=${code}`,
 
     (err, response) => {
-      const url = 'https://api.github.com/user?' + response.body;
+      const url = `https://api.github.com/user?${  response.body}`;
 
       const options = {
         url,
-        headers: {
+headers: {
           'User-Agent': 'request',
-          Accept: 'application/vnd.github.v3+json'
-        }
+          Accept: 'application/vnd.github.v3+json',
+        },
       };
 
       request.get(options, (error, data) => {
         console.log(data);
       });
-    }
+    },
   );
 });
+
+app.post('/parse', (req, res) => {
+  const { program } = req.body;
+  console.log(program);
+  const script = esprima.parseScript(program);
+  // script.body.forEach((element) => {
+  //   console.log('type', element.type);
+  //   console.log('expression', element.expression);
+  //   console.log('arguments', element.expression.arguments);
+    console.log('arg1', element.expression.arguments[0].body.body[0].expression);
+  //   console.log('arg2', element.expression.arguments[1].value);
+
+  // });
+});
+
 
 app.listen(3000);
 
