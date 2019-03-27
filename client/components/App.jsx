a6c4fd6843059c38ee669d7c122586debce7c4a3
Daniel Shu <shudaniel95@gmail.com>
2019-03-26 20:48:38 -0400

    Fixed merge conflicts

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import AppRouter from './AppRouter.jsx';
+import React from 'react';
+
+function App() {
+  console.log("hi")
+  return (
+    <AppRouter></AppRouter>
+  )
+}
+
+export default App;
