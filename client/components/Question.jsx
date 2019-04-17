efe83f6314a50e66a2582f92ff53afba884a5bb0
Daniel Shu <shudaniel95@gmail.com>
2019-04-17 00:18:00 -0400

    Added history redirection

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 const Question = (props) => {
-  console.log(props);
+  // console.log(props);
   const handleClick = (event) => {
     event.preventDefault();
-    props.onQuestionClick(props.id);
+    props.onQuestionClick(props.id, props.user, props.question);
   }
   return (<div onClick={handleClick}>{props.user}: {props.question}</div>)
 }
--- client/components/Thread.jsx
+++ client/components/Thread.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 const Thread = (props) => {
-
-  return (<div>{props}</div>)
+  console.log(props);
+  return (<h1>{props.location.state.user}: {props.location.state.question}</h1>)
 }
 
 export default withRouter(Thread);
